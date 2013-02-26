5b39c87480d62a76d7039c1341d0d869c0e412a7
Sebastian Kreutzer <spacedide@users.sourceforge.net>
2013-02-26 15:29:02 +0000

    [r3]

--- src/spaced/Spaced.java
+++ src/spaced/Spaced.java
@@ -37,7 +37,7 @@ public class Spaced implements VMListener {
 	}
 
 	public static final String TITLE = "Spaced IDE";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.1.0";
 
 	private Properties properties;
 
